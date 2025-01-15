3de15107e3115186f1f9fef23449cf6cd823e43c
Octavasds <121545692+Octavasds@users.noreply.github.com>
2025-01-15 13:16:03 +0200

    Subpunct D

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Service sv = new Service();
-        //sv.start_with();
-        //sv.sortedHouseStark();
+        sv.start_with();
+        sv.sortedHouseStark();
+        sv.anzahl_Ereignise();
     }
 }
--- src/Service.java
+++ src/Service.java
@@ -1,4 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+
 
 public class Service {
     ArrayList<Events> events = FileRepo.readFile();
@@ -29,5 +33,59 @@ public class Service {
         starkHouse.sort(Comparator.comparing(event->event.Datum));
         for(Events ev: starkHouse)
             System.out.println(ev.Datum+" "+ev.MitgliedsName);
+    }
+    public void anzahl_Ereignise(){
+        String[] houses = { "Stark", "Lannister", "Targaryen", "Greyjoy", "Baratheon" };
+        ArrayList<Events> events= new ArrayList<>();
+        events= FileRepo.readFile();
+        int[] points=new int[6];
+        String filePath = "ergebnis.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for(Events ev: events)
+                if(ev.getHaus().equals(houses[0]))
+                    points[0]+=1;
+                else
+                if(ev.getHaus().equals(houses[1]))
+                    points[1]+=1;
+                else
+                if(ev.getHaus().equals(houses[2]))
+                    points[2]+=1;
+                else
+                if(ev.getHaus().equals(houses[3]))
+                    points[3]+=1;
+                else
+                    if(ev.getHaus().equals(houses[4]))
+                        points[4]+=1;
+            String aux;
+            int hpr;
+            for (int i = 0; i < houses.length; i++)
+                for(int j=i+1;j<houses.length;j++)
+                    if(points[i]<points[j]){
+                        hpr=points[i];
+                        points[i]=points[j];
+                        points[j]=hpr;
+                        aux=houses[i];
+                        houses[i]=houses[j];
+                        houses[j]=aux;
+                    }
+            for (int i = 0; i < houses.length; i++)
+                for(int j=i+1;j<houses.length;j++)
+                    if(points[i]==points[j] && houses[i].charAt(0)>houses[j].charAt(0)){
+                        hpr=points[i];
+                        points[i]=points[j];
+                        points[j]=hpr;
+                        aux=houses[i];
+                        houses[i]=houses[j];
+                        houses[j]=aux;
+                    }
+            for(int i=0;i<houses.length;i++)
+            {
+                String line = houses[i] + "#" + points[i];
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
